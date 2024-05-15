acd61bf636287bf823c9272fc9f0dc9e6b5c7dde
Raheel Masood <raheelwp@gmail.com>
2024-05-15 14:53:21 +0400

    push

--- stage/main.tf
+++ stage/main.tf
@@ -7,7 +7,7 @@ module "network" {
 module "cluster" {
   source = "../modules/cluster"
 
-  cluster_name            = var.cluster_names
+  cluster_name            = var.cluster_name
   vpc_id                  = module.network.vpc_id
   private_subnets_ids     = module.network.private_subnets_ids
   node_group_name         = var.node_group_name
